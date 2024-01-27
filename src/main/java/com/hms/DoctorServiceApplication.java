fc2d1410f9b2fe74fe44937fe147d624433e7c54
vintagecodes <rraj02668@gmail.com>
2024-01-28 01:43:36 +0530

    Updated fixes DNS and prop

--- src/main/java/com/hms/DoctorServiceApplication.java
+++ src/main/java/com/hms/DoctorServiceApplication.java
@@ -2,9 +2,10 @@ package com.hms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class DoctorServiceApplication {
 
 	public static void main(String[] args) {
